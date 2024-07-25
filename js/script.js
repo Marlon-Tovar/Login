aaa0e50b915ad818c82cd577b03524d279e51c0e
Marlon Tovar <sneidermarlon@gmail.com>
2024-07-25 15:18:29 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+let oLogin = document.querySelector('.login');
+let oCreate = document.querySelector('.create');
+let oContainer = document.querySelector('.container');
+oLogin.onclick = function(){
+  oContainer.classList.add('signinForm');
+}
+oCreate.onclick = function(){
+  oContainer.classList.remove('signinForm');
+}
+// validacion de los elementos de la contraseña
+let passwordField = document.getElementById("tbxPassword");
+let confirmPasswordField = document.getElementById("tbxConfirmPassword");
+let form = document.getElementById("form");
+form.addEventListener("submit", function(event) {
+  if (passwordField.value !== confirmPasswordField.value) {
+    event.preventDefault();
+    alert("Las contraseñas no coinciden");
+  }
+});
+// Mostrar la contraseña
+let passwordInput = document.getElementById("tbxPassword");
+let showPasswordButton = document.getElementById("showPassword");
+let icon = document.getElementById(".fa-sharp");
+let confirmPasswordInput = document.getElementById("tbxConfirmPassword");
+let showConfirmPasswordButton = document.getElementById("showConfirmPassword");
+let passwordIdInput = document.getElementById("tbxPasswordId");
+let showPassworIdButton = document.getElementById("showPasswordId");
+showPasswordButton.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showPasswordButton.style.opacity = 0.2;
+  } else {
+    passwordInput.type = "password";
+    showPasswordButton.style.opacity = 1;
+  }
+});
+showConfirmPasswordButton.addEventListener("click", () => {
+  if (confirmPasswordInput.type === "password") {
+    confirmPasswordInput.type = "text";
+    showConfirmPasswordButton.style.opacity = 0.2;
+  } else {
+    confirmPasswordInput.type = "password";
+    showConfirmPasswordButton.style.opacity = 1;
+  }
+});
+showPassworIdButton.addEventListener("click", () => {
+  if (passwordIdInput.type === "password") {
+    passwordIdInput.type = "text";
+    showPassworIdButton.style.opacity = 0.2;
+  } else {
+    passwordIdInput.type = "password";
+    showPassworIdButton.style.opacity = 1;
+  }
+});
+// campos vacios
+// document.addEventListener("DOMContentLoaded", () => {
+//   let form = document.getElementById("form");
+//   let warnings = document.getElementById("warnings");
+//   form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     let username = document.getElementById("tbxUsername").value;
+//     let password = document.getElementById("tbxPassword").value;
+//     let confirmPassword = document.getElementById("tbxConfirmPassword").value;
+//     let usernameId = document.getElementById("tbxUsernameId").value;
+//     let passwordId = document.getElementById("tbxPasswordId").value;
+//     if (!username || !password || !confirmPassword || !usernameId || !passwordId) {
+//       warnings.textContent = "Todos los campos son obligatorios";
+//     } else {
+//       form.submit();
+//     }
+//   });
+// });
+
+
+
+
+
